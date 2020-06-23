1a09bde138797d5801d830547741de83f288ac49
Alexander Ignatov <yalishanda@abv.bg>
2020-06-23 18:33:10 +0300

    Add test button

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log(abcjs.signature); // TEST
+function onClickTestButton(event) {
+    console.log("Test: " + ABCJS.signature);
+}
+
+(() => {
+    const testButton = document.getElementById("testButton");
+    testButton.addEventListener("click", onClickTestButton);
+})()
