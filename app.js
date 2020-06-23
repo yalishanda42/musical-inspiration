ee32501551a5318fd85740b8f8c7889288a7654b
Alexander Ignatov <yalishanda@abv.bg>
2020-06-23 19:07:25 +0300

    Add midi player test

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 function onClickTestButton(event) {
     const testAbcString = document.getElementById("testTextarea").value;
-    ABCJS.renderAbc("sheetmusic", testAbcString, { });
+
+    abcjs.renderAbc("sheetmusic", testAbcString, { });
+    abcjs.renderMidi(
+    "midiplayer",
+    testAbcString,
+    {
+        generateDownload: true,
+        inlineControls: {
+            loopToggle: true,
+            startPlaying: true,
+            tempo: true,
+        },
+    });
+    abcjs.midi.startPlaying(document.querySelector(".abcjs-inline-midi"));
+    // abcjs.midi.stopPlaying();
+    // abcjs.midi.restartPlaying();
+    abcjs.midi.setLoop(document.querySelector(".abcjs-inline-midi"), true);
+    // abcjs.midi.setRandomProgress(0.33);
+    //abcjs.midi.setSoundFont("");
 }
 
 (() => {
