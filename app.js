4b18c0e1361913ffd1f22266c0a382ac8b97b047
Alexander Ignatov <yalishanda@abv.bg>
2020-06-23 23:21:12 +0300

    Try with synthcontroller

--- app.js
+++ app.js
@@ -1,44 +1,54 @@
+function generateAbcString() {
+    // TODO: implement algorithm
+    return `X:0
+M:3/4
+L:1/8
+Q:1/4=120
+R:Klezmer
+K:Dm
+Ade|:"Dm"(f2d)e gf|"A7"e2^c4|"Gm"B>>^c BA BG|"A"A3Ade|"Dm"(f2d)e gf|"A7"e2^c4|
+"Gm"A>>B "A7"AG FE|1"Dm"D3Ade:|2"Dm"D3DEF||:"Gm"(G2D)E FG|"Dm"A2F4|"Gm"B>>c "A7"BA BG|
+"Dm"A3 DEF|"Gm"(G2D)EFG|"Dm"A2F4|"E°"E>>Fy "(A7)"ED^C2|1"Dm"D3DEF:|2"Dm"D6||`;
+}
+
 function onClickTestButton(event) {
-    // const testAbcString = document.getElementById("abcTextArea").value;
+    var abcString = generateAbcString();
 
-    // abcjs.renderAbc("sheetmusic", testAbcString, { });
-    // abcjs.renderMidi(
-    //     "midiplayer",
-    //     testAbcString,
-    //     {
-    //         generateDownload: true,
-    //         inlineControls: {
-    //             loopToggle: true,
-    //             startPlaying: true,
-    //             tempo: true,
-    //         },
-    //     }
-    // );
-    // abcjs.midi.startPlaying(document.querySelector(".abcjs-inline-midi"));
-    // abcjs.midi.stopPlaying();
-    // abcjs.midi.restartPlaying();
-    // abcjs.midi.setLoop(document.querySelector(".abcjs-inline-midi"), true);
-    // abcjs.midi.setRandomProgress(0.33);
-    //abcjs.midi.setSoundFont("");
+    var cursorControl = { /* nani? */ };
+    var abcOptions = { add_classes: true };
+    var audioParams = { chordsOff: true };
 
-    new ABCJS.Editor("absTextArea", { paper_id: "sheetmusic",
-        synth: {
-            el: "#midiplayer",
-            options: {
-                displayLoop: true,
-                 displayRestart: true,
-                 displayPlay: true,
-                 displayProgress: true,
-                 displayWarp: true
-             }
-        },
-        generate_warnings: false,
-        // warnings_id:"warnings",
-        abcjsParams: {
-            generateDownload: true,
-            clickListener: null
+    var visualObj = ABCJS.renderAbc("sheetmusic", abcString, abcOptions);
+
+    if (!ABCJS.synth.supportsAudio()) {
+        document.querySelector("#midiplayer").innerHTML = "Audio is not supported in this browser.";
+        return;
+    }
+
+    var synthController = new ABCJS.synth.SynthController();
+    synthController.load("#midiplayer",
+        cursorControl,
+        {
+            displayLoop: true,
+            displayRestart: true,
+            displayPlay: true,
+            displayProgress: true,
+            displayWarp: true
         }
-    });
+    );
+
+    var createSynth = new ABCJS.synth.CreateSynth();
+    createSynth.init({ visualObj: visualObj[0] })
+        .then(() => {
+            synthController.setTune(visualObj[0], false, audioParams)
+                .then(() => {
+                    console.log("Audio successfully loaded.")
+                }).catch((error) => {
+                    console.warn("Audio problem:", error);
+                });
+        }).catch((error) => {
+            console.warn("Audio problem:", error);
+        });
 }
 
 (() => {
